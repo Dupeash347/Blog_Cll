1c1b8c326938e05bdfd1502fe7dd1524dc2926e4
johnson <ashirwadayirumala@gmial.com>
2024-02-15 21:17:35 +0530

    Made Changes

--- blog/settings.py
+++ blog/settings.py
@@ -123,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
